5c6dddef4459ea44abdfd7028bb9cda2932b0b72
priyanshu-Raj26 <er.priyansu@gmail.com>
2024-09-17 00:57:17 +0530

    Initial commit

---
+++ ASCII.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char name[] = "priyanshu";
+    int arr[10],i;
+
+    for (i = 0; name[i]!='\0'; i++)
+    {
+        printf("%c=%d\t",name[i],name[i]);
+        arr[i] = name[i];
+    }
+    printf("\n");
+    for (i = 0; i<9; i++)
+    {
+         printf("%d\t",arr[i]);
+    }
+    
+    return 0;
+}
